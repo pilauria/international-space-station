a5bed8d7bf8b9427f33665ce47e687e6ec2f3265
Pietro Lauria <ptr.lauria@gmail.com>
2021-06-29 17:54:24 +0200

    Initial commit

---
+++ dist/script.js
@@ -0,0 +1,19 @@
+const button = document.querySelector('.button');
+
+function getPosition() {
+  fetch('http://api.open-notify.org/iss-now.json')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      var date = new Date();
+      data = `<h2>Current Position</h2>
+              <p>Latitute: (${data.iss_position.latitude})</p>
+              <p>Longitude: ${data.iss_position.longitude})</p>
+              <p>Date: [${date}]</p>`;
+
+      document.getElementById('output').innerHTML = data;
+      // button.style.width = '50%';
+    });
+}
+
+button.addEventListener('click', getPosition);
